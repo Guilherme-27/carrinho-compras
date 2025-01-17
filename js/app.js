78f58c5d50b3419719840dc03c6abffc9f2b11f1
Guilherme Gabriel <guilhermegabriel.mf@gmail.com>
2025-01-17 11:18:15 -0300

    revisado

--- js/app.js
+++ js/app.js
@@ -1,31 +1,38 @@
 let total = document.getElementById('valor-total');
 let listaProdutos = document.getElementById('lista-produtos');
-let valorTotal = 1400;
+let valorTotal;
+limpar();
 
 function adicionar() {
     let produto = document.getElementById('produto').value;
     let quantidade = document.getElementById('quantidade').value;
     let valorProduto;
     let nomeProduto;
-
-    switch (produto) {
-        case 'Fone de ouvido - R$100':
-            valorProduto = 100*quantidade;
-            nomeProduto = 'Fone de ouvido';
-            adicionarCarrinho(quantidade, nomeProduto, valorProduto)
-            break;
-        case 'Celular - R$1400':
-            valorProduto = 1400*quantidade;
-            nomeProduto = 'Celular';
-            adicionarCarrinho(quantidade, nomeProduto, valorProduto);
-            break;
-        case 'Oculus VR - R$5000':
-            valorProduto = 5000*quantidade;
-            nomeProduto = 'Oculus VR';
-            adicionarCarrinho(quantidade, nomeProduto, valorProduto);
-            break;
+    // Outra alternativa para o problema seria analisar a variável produto, usando o split('-') e split('R$') para separar nome e valor do produto
+    // Essa abordagem seria mais eficiente caso precisássemos adicionar mais produtos à página
+    if (quantidade > 0) {
+        switch (produto) {
+            case 'Fone de ouvido - R$100':
+                valorProduto = 100*quantidade;
+                nomeProduto = 'Fone de ouvido';
+                adicionarCarrinho(quantidade, nomeProduto, valorProduto);
+                break;
+            case 'Celular - R$1400':
+                valorProduto = 1400*quantidade;
+                nomeProduto = 'Celular';
+                adicionarCarrinho(quantidade, nomeProduto, valorProduto);
+                break;
+            case 'Oculus VR - R$5000':
+                valorProduto = 5000*quantidade;
+                nomeProduto = 'Oculus VR';
+                adicionarCarrinho(quantidade, nomeProduto, valorProduto);
+                break;
+        }
+        document.getElementById('quantidade').value = null;
+    } else {
+        alert('A quantidade precisar ser maior que 0');
     }
-
+    
 }
 
 function limpar() {
